f5d2944df8d329ee869b497b5b8c455bcfa2c78f
Obianonwo Chinedu <56441231+Gingercapo@users.noreply.github.com>
2022-09-06 03:12:17 +0100

    create concat sql

---
+++ CONCAT.sql
@@ -0,0 +1,20 @@
+Quizzes CONCAT
+-- question1 
+-- Each company in the accounts table wants to create an email address for each primary_poc. The email address should be the first name of the primary_poc . last name primary_poc @ company name .com.
+WITH t1 AS (
+ SELECT 
+    LEFT(primary_poc,     position(' ' IN primary_poc)) first_name,       RIGHT(primary_poc, LENGTH(primary_poc) - position(' ' IN     	     primary_poc)) last_name, name
+ FROM accounts)
+SELECT first_name, last_name, CONCAT(first_name, '.', last_name, '@', name, '.com')
+FROM t1;
+
+
+
+-- question2
+-- You may have noticed that in the previous solution some of the company names include spaces, which will certainly not work in an email address. See if you can create an email address that will 
+-- work by removing all of the spaces in the account name, but otherwise your solution should be just as in question 1. Some helpful documentation is here.
+
+
+--question3 
+-- We would also like to create an initial password, which they will change after their first log in. The first password will be the first letter of the primary_poc's first name (lowercase), then the last letter of their first name (lowercase), the first letter of their last name (lowercase), the last letter of their last name (lowercase), the number 
+-- of letters in their first name, the number of letters in their last name, and then the name of the company they are working with, all capitalized with no spaces.
