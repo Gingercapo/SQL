66a5b0a7ef851f9fd96a16254b36cbac6472cb29
Obianonwo Chinedu <56441231+Gingercapo@users.noreply.github.com>
2022-09-06 01:39:42 +0100

    Update sql.sql

--- sql.sql
+++ sql.sql
@@ -26,3 +26,13 @@ FROM (SELECT name, CASE WHEN LEFT(UPPER(name), 1) IN ('0','1','2','3','4','5','6
       --There are 350 company names that start with a letter and 1 that starts with a number. This gives a ratio of 350/351 that are company names that start with a letter or 99.7%.
 
 --question4. Consider vowels as a, e, i, o, and u. What proportion of company names start with a vowel, and what percent start with anything else?
+
+SELECT SUM(vowels) vowels, SUM(other) other
+FROM (SELECT name, CASE WHEN LEFT(UPPER(name), 1) IN ('A','E','I','O','U') 
+                        THEN 1 ELSE 0 END AS vowels, 
+          CASE WHEN LEFT(UPPER(name), 1) IN ('A','E','I','O','U') 
+                       THEN 0 ELSE 1 END AS other
+         FROM accounts) t1;
+         
+         --soluton
+-- There are 80 company names that start with a vowel and 271 that start with other characters. Therefore 80/351 are vowels or 22.8%. Therefore, 77.2% of company names do not start with vowels.
