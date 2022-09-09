3b437585a5b0fe157ce6c2c21b709c6490b52f7e
Obianonwo Chinedu <56441231+Gingercapo@users.noreply.github.com>
2022-09-10 00:07:43 +0100

    update

--- GROUP_BY_sql.sql
+++ GROUP_BY_sql.sql
@@ -98,9 +98,6 @@ FROM orders o
                 GROUP BY 1
 
 #9. Determine the number of times a particular channel was used in the web_events table for each sales rep. Your final table should have three columns - the name of the sales rep, the channel, and the number of occurrences. Order your table with the highest number of occurrences first.
-
-
-#10. Determine the number of times a particular channel was used in the web_events table for each region. Your final table should have three columns - the region name, the channel, and the number of occurrences. Order your table with the highest number of occurrences first.
 SELECT 
 	r.name, 
 	channel, count(*) count_channel
@@ -110,6 +107,17 @@ FROM web_events w
 	       Group BY channel, name
 		 ORDER BY 3 
 
+#10. Determine the number of times a particular channel was used in the web_events table for each region. Your final table should have three columns - the region name, the channel, and the number of occurrences. Order your table with the highest number of occurrences first.
+SELECT r.name, w.channel, COUNT(*) num_events
+FROM accounts a
+     JOIN web_events w
+          ON a.id = w.account_id
+     JOIN sales_reps s
+          ON s.id = a.sales_rep_id
+     JOIN region r
+          ON r.id = s.region_id
+            GROUP BY r.name, w.channel
+              ORDER BY num_events DESC;
 #11. For each account, determine the average amount of each type of paper they purchased across their orders. Your result should have four columns - one for the account name and one for the average quantity purchased for each of the paper types for each account.
 SELECT 
 	a.name,
