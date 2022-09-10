9a929c34206d2baf41d8cbba10709fbe21a864bf
Obianonwo Chinedu <56441231+Gingercapo@users.noreply.github.com>
2022-09-10 01:04:31 +0100

    Update Distinct_sql.sql

--- Distinct_sql.sql
+++ Distinct_sql.sql
@@ -11,3 +11,16 @@ FROM accounts a
        ON s.id = a.sales_rep_id
     JOIN region r
        ON r.id = s.region_id;
+       
+       
+#2. Have any sales reps worked on more than one account?
+
+#Actually all of the sales reps have worked on more than one account. The fewest number of accounts any sales rep works on is 3. There are 50 sales reps, and they all have more than one account.
+#Using DISTINCT in the second query assures that all of the sales reps are accounted for in the first query.       
+       
+SELECT s.id, s.name, count(*) no_acc
+FROM sales_reps s
+LEFT JOIN accounts a
+		ON s.id = a.sales_rep_id
+        group by 1,2
+        order by 3 desc
