81ba23cd806e98a48ee725227bfb2765a5b62d6a
Obianonwo Chinedu <56441231+Gingercapo@users.noreply.github.com>
2022-09-10 01:07:34 +0100

    Update Distinct_sql.sql

--- Distinct_sql.sql
+++ Distinct_sql.sql
@@ -17,7 +17,10 @@ FROM accounts a
 #Actually all of the sales reps have worked on more than one account. The fewest number of accounts any sales rep works on is 3. There are 50 sales reps, and they all have more than one account.
 #Using DISTINCT in the second query assures that all of the sales reps are accounted for in the first query.       
        
-SELECT s.id, s.name, COUNT(*) no_acc
+SELECT 
+	s.id, 
+	s.name, 
+	COUNT(*) no_acc
 FROM sales_reps s
 LEFT JOIN accounts a
 	ON s.id = a.sales_rep_id
